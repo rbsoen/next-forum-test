7e3bbd01ab48e10abd79279f675aca7424f57d5f
Rodriguez Breil Soenoto <rbsoenoto@gmail.com>
2023-10-09 22:32:19 +0700

    fix: wrong attr name

--- src/app/thread/new/page.tsx
+++ src/app/thread/new/page.tsx
@@ -6,7 +6,7 @@ import { poppinsBold } from "@/app/fonts";
 
 export default async function Page() {
     return <Layout>
-        <h2 class={`staticH2 ${poppinsBold.className}`}>
+        <h2 className={`staticH2 ${poppinsBold.className}`}>
             Start a conversation&hellip;
         </h2>
         <Post editable={true} post={null}/>
